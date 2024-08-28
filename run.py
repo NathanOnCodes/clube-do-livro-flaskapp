2b72e7129bb6a43011bcda7ac181601ac6347b1c
NathanOnCodes <contatonathancf@gmail.com>
2024-08-28 20:33:07 -0300

    refactor: renomeando importações

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 from app import create_app
 from app import db
+from flask.cli import with_appcontext
+import click
 
 app = create_app()
 
-with app.app_context():
+@click.command(name="create_tables")
+@with_appcontext
+def create_tables():
     db.create_all()
+
+
+app.cli.add_command(create_tables)
 
 if __name__ == "__main__":
     app.run(debug=True)
